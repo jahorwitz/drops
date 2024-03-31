04f28bafe71064c633a7493807cc1fcff0a81716
Dillona25 <Dillyb00by49@gmail.com>
2024-03-31 11:16:43 -0500

    refactor: removie stories dir

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -3,7 +3,7 @@ import { FormEventHandler, ReactNode } from "react";
 // Types for the props on the form
 type formProps = {
   className?: string;
-  children?: ReactNode; // ReactNode is anything that can be rendered in a React Component
+  children?: ReactNode;
   onSubmit?: FormEventHandler;
 };
 
@@ -14,5 +14,3 @@ export const Form = ({ className, children, onSubmit, ...rest }: formProps) => {
     </form>
   );
 };
-
-// Any created form compound components should be placed below here. Exc "Form.TextInput"
