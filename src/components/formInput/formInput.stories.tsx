c9f70304dec47a72423b382454bd51279c95d131
Dillona25 <Dillyb00by49@gmail.com>
2024-04-01 13:02:56 -0500

    fix: removed code meant for seperate branch

---
+++ src/components/formInput/formInput.stories.tsx
@@ -0,0 +1,10 @@
+import { FormInput } from "./formInput";
+
+export default {
+  title: "textInput",
+  component: FormInput,
+};
+
+export const FormInputStory = () => {
+  return <FormInput></FormInput>;
+};
