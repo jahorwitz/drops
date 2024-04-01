7419d929c5e419ca013d9532fbc6ddfbdc961f0d
iankamar <ian.kamar@outlook.com>
2024-04-01 16:49:00 -0400

    fix(form-radiogroup): update

--- src/components/formRadioGroup/formRadioGroup.tsx
+++ src/components/formRadioGroup/formRadioGroup.tsx
@@ -29,8 +29,9 @@ export const RadioGroup = ({title, description, options}: RadioGroupProps) => {
             <div {...register('inputText', {required: true})}>
                 {options.map((option, index) => (
                     <label key={index}>
-                        <input type="radio" value={option}/>
+                        <input type="radio" value={option}>
                         {option}
+                        </input>
                     </label>
                 ))}
             </div>
--- src/components/formRadioGroup/index.ts
+++ src/components/formRadioGroup/index.ts
@@ -1,1 +1,5 @@
+<<<<<<< HEAD:src/components/formRadioGroup/index.ts
 export * from "./formRadioGroup"
+=======
+export * from "./form.RadioGroup"
+>>>>>>> f27a00f (fix(form-radiogroup): update):src/components/form.RadioGroup/index.ts
