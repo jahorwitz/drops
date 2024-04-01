8a1b97d94e8c6257ba0f7e008a68e3657481b852
iankamar <ian.kamar@outlook.com>
2024-04-01 16:54:21 -0400

    fix(form-radiogroup): update

--- src/components/formRadioGroup/index.ts
+++ src/components/formRadioGroup/index.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD:src/components/formRadioGroup/index.ts
-export * from "./formRadioGroup"
-=======
 export * from "./form.RadioGroup"
->>>>>>> f27a00f (fix(form-radiogroup): update):src/components/form.RadioGroup/index.ts
