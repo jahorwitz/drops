0fc2c2afed25b88fdd67aee9db252810655dba64
iankamar <ian.kamar@outlook.com>
2024-04-01 03:36:27 -0400

    feat(form): add input validation

---
+++ src/components/form/form.RadioGroup.tsx
@@ -0,0 +1,40 @@
+import { Form } from "./form";
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+    diabetesType: string;
+};
+
+const DiabetesForm = () => {
+    const {register, handleSubmit, formState: { errors }} = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        if (!errors.diabetesType) {
+        alert(`You selected ${data.diabetesType}`)
+        } else {
+            alert( 'Please select a diabetes type');
+        }
+    };
+
+    return (
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <p>Choose your diabetes type</p>
+            <div {...register('diabetesType', {required: true})}>
+                <label>
+                    <input type= "radio" value= "Type 1"/>
+                    Type 1
+                </label>
+                <label>
+                    <input type= "radio" value= "Type 2"/>
+                    Type 2
+                </label>
+                <label>
+                    <input type= "radio" value= "Gestational"/>
+                    Gestational
+                </label>
+            </div>
+        </Form>
+    );
+};
+
+export default DiabetesForm;
