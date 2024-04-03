700528678c3f661f64b93ad1ac63314aa6fb7755
iankamar <ian.kamar@outlook.com>
2024-04-03 12:24:47 -0400

    fix(formRadioGroup): update

--- src/components/formRadioGroup/formRadioGroup.tsx
+++ src/components/formRadioGroup/formRadioGroup.tsx
@@ -24,17 +24,25 @@ export const RadioGroup = ({title, description, options}: RadioGroupProps) => {
     };
 
     return (
-        <Form onSubmit={handleSubmit(onSubmit)}>
-            <p>{title}</p>
-            <div {...register('inputText', {required: true})}>
+        <Form onSubmit={handleSubmit(onSubmit)} className="space-y-3 text-black max-w-pageContent w-86 h-auto">
+            <p className="section-subtext">{title}</p>
+            <div className="space-y-2">
                 {options.map((option, index) => (
-                    <label key={index}>
-                        <input type="radio" value={option}/>
-                        {option}
+                    <label key={index} className="flex items-center space-x-2 border-2 border-lightGray rounded-lg p-3 text-paragraph-lg"  style={{
+                        borderColor: selectedOption === option ? '#121212' : '#f5f5f5',
+                    }}>
+                        <input 
+                            type="radio" 
+                            value={option} 
+                            checked={selectedOption === option}
+                            onChange={() => handleOptionChange(option)}
+                            className={`appearance-none h-4 w-4 border-4 bold-border-black rounded-full checked:border-black focus:outline-none focus:ring 1 focus:ring-black ${selectedOption === option ? 'bg-white border-black' : 'bg-white border-lightGray'}`}
+                        />
+                        <span className="text-paragraph-lg text-black-400">{option}</span>
                     </label>
                 ))}
             </div>
-            <p className="hint-text">{description}</p>
+            <p className="text-paragraph-lg text-lightGray-400">{description}</p>
         </Form>
     );
 };
