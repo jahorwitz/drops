5638e13b730f6d2c4cf3d9b6af9857c5456f510c
iankamar <ian.kamar@outlook.com>
2024-04-03 12:12:53 -0400

    fix(formRadioGroup): update

---
+++ src/components/formRadioGroup/formRadioGroup.tsx
@@ -0,0 +1,40 @@
+import { Form } from "../form";
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+    inputText: string;
+};
+
+
+type RadioGroupProps = {
+    title: string;
+    description: string;
+    options: string[];
+}
+
+export const RadioGroup = ({title, description, options}: RadioGroupProps) => {
+    const {register, handleSubmit, formState: { errors }} = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        if (!errors.inputText) {
+        alert(`You selected ${data.inputText}`)
+        } else {
+            alert( 'Please select an option');
+        }
+    };
+
+    return (
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <p>{title}</p>
+            <div {...register('inputText', {required: true})}>
+                {options.map((option, index) => (
+                    <label key={index}>
+                        <input type="radio" value={option}/>
+                        {option}
+                    </label>
+                ))}
+            </div>
+            <p className="hint-text">{description}</p>
+        </Form>
+    );
+};
