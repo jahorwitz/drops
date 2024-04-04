678cccddc56e4da71375f361d166c1215f927847
Divya B <divyaaa1812@gmail.com>
2024-04-03 21:12:53 -0700

    style(fonts): text color fixed

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler, ReactNode } from "react";
+import { MouseEventHandler } from "react";
 
 type buttonProps = {
     
@@ -13,15 +13,15 @@ export const ButtonPrimary = ({buttonText="value", onClick}:buttonProps) => {
     )
 }
 
-export const ButtonSecondary = ({buttonText, onClick}:buttonProps) => {
+export const ButtonSecondary = ({buttonText="text", onClick}:buttonProps) => {
     return (
         <button className= "text-[#121212] bg-transparent px-10 border-[#121212] border rounded-lg grow-0 h-14 w-80 text-lg font-medium font-text" onClick={onClick}>{buttonText}</button>
     )
 }
 
-export const ButtonText = ({buttonText, onClick}:buttonProps) => {
+export const ButtonText = ({buttonText="value", onClick}:buttonProps) => {
         return (
-            <button className = "text-[#121212] opacity-60 text-lg font-medium font-text" onClick={onClick}>{buttonText}</button>
+            <button className = "text-[#121212] opacity-60 grow-0 h-14 w-80 text-lg font-medium font-text" onClick={onClick}>{buttonText}</button>
         )
 }
 
