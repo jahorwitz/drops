b8bf895ebeb7808ad6d01e29958c29e39f0fc1e6
Divya B <divyaaa1812@gmail.com>
2024-04-04 20:48:37 -0700

    style: added classname var for button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,29 +5,30 @@ type buttonProps = {
     buttonText?: string;
     onClick?: MouseEventHandler;
     icon?:string;
+    className?:string;
   };
 
-export const ButtonPrimary = ({buttonText="value", onClick}:buttonProps) => {
+export const ButtonPrimary = ({buttonText="value", onClick, className}:buttonProps) => {
     return (
-        <button className="text-lightGray bg-[#121212] px-10 rounded-lg grow-0 h-14  w-80 text-lg font-medium font-text" onClick={onClick}>{buttonText}</button>
+        <button className={`text-lightGray bg-[#121212] px-10 rounded-lg grow-0 h-14  w-80 text-lg font-medium font-text ${className}`} onClick={onClick}>{buttonText}</button>
     )
 }
 
-export const ButtonSecondary = ({buttonText="text", onClick}:buttonProps) => {
+export const ButtonSecondary = ({buttonText="text", onClick, className}:buttonProps) => {
     return (
-        <button className= "text-[#121212] bg-transparent px-10 border-[#121212] border rounded-lg grow-0 h-14 w-80 text-lg font-medium font-text" onClick={onClick}>{buttonText}</button>
+        <button className= {`text-[#121212] bg-transparent px-10 border-[#121212] border rounded-lg grow-0 h-14 w-80 text-lg font-medium font-text ${className}`} onClick={onClick}>{buttonText}</button>
     )
 }
 
-export const ButtonText = ({buttonText="value", onClick}:buttonProps) => {
+export const ButtonText = ({buttonText="value", onClick, className}:buttonProps) => {
         return (
-            <button className = "text-[#121212] opacity-60 grow-0 h-14 w-80 text-lg font-medium font-text" onClick={onClick}>{buttonText}</button>
+            <button className = {`text-[#121212] opacity-60 grow-0 h-14 w-80 text-lg font-medium font-text ${className}`} onClick={onClick}>{buttonText}</button>
         )
 }
 
-export const ButtonIcon = ({icon, onClick}:buttonProps) => {
+export const ButtonIcon = ({icon, onClick, className}:buttonProps) => {
     return (
-        <button className="w-8 h-8" onClick={onClick}>
+        <button className={`w-8 h-8 ${className}`} onClick={onClick}>
             <img className="p-1" src = {icon}/>
         </button>
     )
