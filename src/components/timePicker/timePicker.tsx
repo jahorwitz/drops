eddc8bec7e7fae1bc809bdfa9230f3783867e93d
maryafzali24 <123387020+maryafzali24@users.noreply.github.com>
2024-04-05 09:51:38 -0400

    style: add font and fix dimension

--- src/components/timePicker/timePicker.tsx
+++ src/components/timePicker/timePicker.tsx
@@ -5,38 +5,40 @@ type TimePickerProps = {
 }
 const TimePicker = ({ label }: TimePickerProps) => {
   const { register, formState: { errors } } = useForm();
+
+  const inputClassName = "text-center w-[60px] h-[60px] border border-solid border-gray-400 bg-gray-100 rounded-lg";
   return (
-    <div className="w-346 h-99">
-    <label className="text-base leading-[19.2px] font-rubik mb-4">{label}</label>
-    <div className="h-76 bg-gray-100 rounded-lg flex items-center p-4">
-     <input type="number"
+  <div>
+    <label className="text-base leading-[19px] font-text mb-1">{label}</label>
+    <div className="h-[76px] bg-gray-100 rounded-lg flex items-center p-2">
+     <input type="text"
   {...register('hour', {
     required: true,
     min: 0,
     max: 12,
     pattern: /^(0?[0-9]|1[0-2])$/,
 })}
-  className="w-60 h-60 border border-solid border-[1px] border-gray-400 bg-gray-100 rounded-lg text-center" />
-      <span className="ml-[12.5px]">:</span>
-      <input type="number"
+  className={inputClassName}/>
+     <span className="ml-[12px] mr-3">:</span>
+     <input type="text"
   {...register('minutes', {
     required: true,
     min: 0.0,
     max: 59,
     pattern: /^(0?[0-9]|[1-5][0-9]|60)$/,
-  })} className="w-60 h-60 border border-solid border-[1px] border-gray-400 bg-gray-100 rounded-lg text-center mx-[12.5px]" />
- 
-        <select
-          {...register('period', {required: true})}
-          className="w-60 h-60 border border-solid border-[1px] border-gray-400 bg-gray-100 rounded-lg mx-[1px] text-center"
-        >
-          <option value="AM">AM</option>
-          <option value="PM">PM</option>
-        </select>
-        </div>
-      {errors.hour && <p className="text-red-600 ml-2">Please enter a valid hour (00-12)</p>}
-      {errors.minutes && <p className="text-red-600 ml-2">Please enter a valid minutes (00-59)</p>}
-      {errors.period && <p className="text-red-600 ml-2">Please select a period (AM/PM)</p>}
+  })} className={`${inputClassName} mr-2`}/>
+     <input
+          type="text"
+          {...register('period', {
+            required: true,
+            pattern: /^(AM|PM)$/i, // Case insensitive pattern matching for AM or PM
+          })}
+          className={`${inputClassName} font-text`}
+        />
+      </div>
+      {errors.hour && <p className="text-red-600 ml-2 font-text">Please enter a valid hour (00-12)</p>}
+      {errors.minutes && <p className="text-red-600 ml-2 font-text">Please enter a valid minutes (00-59)</p>}
+      {errors.period && <p className="text-red-600 ml-2 font-text">Please select a period (AM/PM)</p>}
     </div>
   );
 };
