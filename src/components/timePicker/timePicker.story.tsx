dddfefb0879ebae549e43523a5d84f95f7d4aade
maryafzali24 <123387020+maryafzali24@users.noreply.github.com>
2024-04-06 16:52:11 -0400

    feat: added time-picker component

---
+++ src/components/timePicker/timePicker.story.tsx
@@ -0,0 +1,16 @@
+import TimePicker from "./timePicker";
+
+export default {
+    title: "TimePicker",
+    component: TimePicker,
+    // Configuring arg types for controls in Storybook
+    argTypes: {
+        label: { control: "text"} // Configuring a control for the label prop
+    }
+}
+
+export const TimePickerStory = (args: { label?: string }) => <TimePicker {...args} />;
+// Setting initial args for the TimePickerStory
+TimePickerStory.args = {
+    label: "Reminder 1",
+}
