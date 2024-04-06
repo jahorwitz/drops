339df6c95725a666a374f99d4c3dd2477c76de56
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-04-06 06:49:26 -0700

    style: comments for code

--- src/components/form/form.feedback.tsx
+++ src/components/form/form.feedback.tsx
@@ -1,7 +1,9 @@
+// props for form feedback
 interface FormFeedbackProps {
     message?: string;
 }
 
+// creating the component with message
 const formFeedback = ({ message }: FormFeedbackProps) => {
     return message ? <div>{message}</div> : null;
 }
