987e078e91af0e64096830154c293c1a28cede04
Divya B <divyaaa1812@gmail.com>
2024-06-05 15:25:48 -0700

    feat: update file name

--- src/components/Goal/goal.stories.tsx
+++ src/components/Goal/goal.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import ExerciseGoal, { ExerciseGoalProps } from './exercisegoal';
+import ExerciseGoal, { ExerciseGoalProps } from './goal';
 import { GoalType } from '../../utils/goal';
 
 export default {
