2f761e50c036693f4a84903e9ea6dffb4412fc97
Cristofer Ledesma <ledes7080@gmail.com>
2024-08-10 21:50:34 -0400

    feat: update the Step-wizard

--- src/components/stepWizard/stepWizard.stories.tsx
+++ src/components/stepWizard/stepWizard.stories.tsx
@@ -3,13 +3,43 @@ import { StepWizard } from "./stepWizard";
 
 export default {
   title: "StepWizard",
-  component: StepWizard,
+  component: StepWizard, StepWizardProvider,
 };
 
 export const StepWizardStory = () => {
+
   return (
     <StepWizardProvider>
-      <StepWizard title="Setting up">{/* Steps here */}</StepWizard>
+      <StepWizard title="Setting up" > 
+        <p>
+        Setting up your desired glucose levels
+        </p>
+        </StepWizard>
+        <StepWizard title="Setting up">
+          <p>
+            Get notified for your glucose levels
+          </p>
+        </StepWizard>
+        <StepWizard title="Setting up">
+          <p>
+            Setting up your medication reminders
+          </p>
+        </StepWizard>
+        <StepWizard title="Setting up">
+          <p>
+            Setting up exercise goals
+          </p>
+        </StepWizard>
+        <StepWizard title="Setting up">
+          <p>
+            Setting up diet goals
+          </p>
+        </StepWizard>
+        <StepWizard title="Setting up">
+          <p>
+          Setting up diet goals reminders
+          </p>
+        </StepWizard>
     </StepWizardProvider>
   );
 };
