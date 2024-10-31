429067709fa4ef9d838450b618ee414eb13ae6bf
Riley Marcum <rmarcum13@gmail.com>
2024-10-30 22:39:08 -0400

    feat: setup for login route

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./global/default.css";
-import { GlucoseNotificationPrompt, Start, Welcome, RegistrationConfirmation } from "./routes";
+import {
+  GlucoseNotificationPrompt,
+  Start,
+  Welcome,
+  Login,
+  RegistrationConfirmation,
+} from "./routes";
 
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
@@ -16,12 +22,19 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<div>Home Route</div>} />
-          <Route path="/registration-confirm" element={<RegistrationConfirmation />} />
-          <Route path="/onboarding/glucose-notifications" element={<GlucoseNotificationPrompt />} />
+          <Route
+            path="/registration-confirm"
+            element={<RegistrationConfirmation />}
+          />
+          <Route
+            path="/onboarding/glucose-notifications"
+            element={<GlucoseNotificationPrompt />}
+          />
           <Route path="/welcome" element={<Welcome />} />
           <Route path="/onboarding" element={<Start />} />
-        </Routes >
-      </BrowserRouter >
-    </ApolloProvider >
-  </React.StrictMode >
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
+  </React.StrictMode>,
 );
