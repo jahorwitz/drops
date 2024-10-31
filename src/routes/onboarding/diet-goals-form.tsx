4891f03e73ab7861236a9f632d8e781230cea732
Caleb <chasch96@gmail.com>
2024-10-31 21:50:33 +0800

    feat: add diet-goals-form route & component

---
+++ src/routes/onboarding/diet-goals-form.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Form } from "../../components"
+
+export const DietGoalsForm: React.FC = () => {
+  return (
+    <div>
+      <p>Test</p>
+      <Form></Form>
+    </div>
+  )
+}
