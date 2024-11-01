478e42d667a6fe566fef0a1a76f70e7947e9e3f0
Riley Marcum <rmarcum13@gmail.com>
2024-10-31 23:18:24 -0400

    fix: error messages not displaying

--- src/routes/login/login.tsx
+++ src/routes/login/login.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import logo from "../../images/Logo.svg";
@@ -6,7 +5,7 @@ import backbutton from "../../images/Backbutton.svg";
 import { Button, Form } from "../../components";
 
 export const Login: React.FC = () => {
-  interface MyFormValues {
+  interface FormValues {
     email: string;
     password: string;
   }
@@ -15,9 +14,9 @@ export const Login: React.FC = () => {
     register,
     handleSubmit,
     formState: { errors, isValid },
-  } = useForm<MyFormValues>();
+  } = useForm<FormValues>();
 
-  const onSubmit = (data: MyFormValues) => {
+  const onSubmit = (data: FormValues) => {
     console.log("submitting");
     alert(JSON.stringify(data, null, 2));
   };
@@ -45,6 +44,7 @@ export const Login: React.FC = () => {
           <Form.TextInput
             labelText="Email"
             placeholder="Enter your email"
+            type="text"
             feedback={errors.email?.message}
             {...register("email", {
               required: "This field is required",
@@ -74,7 +74,6 @@ export const Login: React.FC = () => {
             type="submit"
             buttonText="Log In"
             variant="primary"
-            disabled={!isValid}
             className={`h-[60px] w-full ${
               !isValid ? "pointer-events-none opacity-20" : ""
             }`}
@@ -84,7 +83,6 @@ export const Login: React.FC = () => {
               type="button"
               buttonText="Or register"
               variant="text"
-              disabled={!isValid}
               className="opacity-100 hover:opacity-80 mt-8 p-0 rounded-lg"
             ></Button>
           </Link>
@@ -93,3 +91,59 @@ export const Login: React.FC = () => {
     </div>
   );
 };
+
+{
+  /* <Form
+onSubmit={handleSubmit(onSubmit)}
+className="flex flex-col max-w-[390px] w-full px-4  mt-8 h-full  max-h-[502px] self-center"
+>
+<div className="flex flex-col h-full gap-5">
+  <Form.TextInput
+    labelText="Email"
+    placeholder="Enter your email"
+    type="text"
+    feedback={errors.email?.message}
+    {...register("email", {
+      required: "This field is required",
+      pattern: {
+        value:
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        message: "Please enter a valid email address",
+      },
+    })}
+  />
+  <Form.TextInput
+    labelText="Password"
+    placeholder="Enter your password"
+    type="password"
+    feedback={errors.password?.message}
+    {...register("password", {
+      required: "This field is required",
+      minLength: {
+        value: 8,
+        message: "Password must be at least 8 characters long",
+      },
+    })}
+  />
+</div>
+<div className="flex flex-col items-center">
+  <Button
+    type="submit"
+    buttonText="Log In"
+    variant="primary"
+    disabled={!isValid}
+    className={`h-[60px] w-full ${
+      !isValid ? "pointer-events-none opacity-20" : ""
+    }`}
+  ></Button>
+  <Link to="/register">
+    <Button
+      type="button"
+      buttonText="Or register"
+      variant="text"
+      className="opacity-100 hover:opacity-80 mt-8 p-0 rounded-lg"
+    ></Button>
+  </Link>
+</div>
+</Form> */
+}
