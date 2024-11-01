c39595b8c391ebf66aeb6423341981b22afb7e70
Caleb <chasch96@gmail.com>
2024-11-01 23:49:01 +0800

    feat: add numeric and text inputs

--- src/routes/onboarding/diet-goals-form.tsx
+++ src/routes/onboarding/diet-goals-form.tsx
@@ -1,11 +1,79 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Form } from "../../components"
 
 export const DietGoalsForm: React.FC = () => {
+  interface FormValues {
+    meals: number;
+    snacks: number;
+    carbs: number;
+    fiber: number;
+    water: number;
+    calories: number;
+  }
+
+  const {
+    register,
+    formState: {errors},
+  } = useForm<FormValues>();
+
   return (
     <div>
-      <p>Test</p>
-      <Form></Form>
+      <h2>Set your diet goals</h2>
+      <Form className="flex flex-col gap-4">
+      <Form.NumericInput {...register("meals", {
+          validate: (value) => (value ? (value >= 3 || 'This field must be greater than or equal to 3') : 'This field is required'),
+        })}
+        labelText="Meals per day"
+        hintText="3 is a recommended amount"
+        feedback={errors.meals?.message} />
+      <Form.NumericInput {...register("snacks", {
+          validate: (value) => (value ? (value <= 3 || 'This field must be greater than or equal to 3') : 'This field is required'),
+        })}
+        labelText="Snacks per day"
+        hintText="2 is a recommended amount"
+        feedback={errors.snacks?.message} />
+      <Form.TextInput
+      labelText="Carbs per day (grams)"
+      hintText="135 grams per day is recommended for people of your age"
+      placeholder="Enter your carbs per day"
+      type="text"
+      feedback={errors.carbs?.message}
+      {...register("carbs", {
+        required: "This field is required",
+      })}
+       />
+       <Form.TextInput
+      labelText="Fiber per day (grams)"
+      hintText="25 grams per day is recommended for people of your age"
+      placeholder="Enter your fiber per day"
+      type="text"
+      feedback={errors.fiber?.message}
+      {...register("fiber", {
+        required: "This field is required",
+      })}
+       />
+       <Form.TextInput
+      labelText="Cups of water per day"
+      hintText="15 cups per day is recommended for people of your age"
+      placeholder="Enter your cups of water per day"
+      type="text"
+      feedback={errors.water?.message}
+      {...register("water", {
+        required: "This field is required",
+      })}
+       />
+       <Form.TextInput
+      labelText="Calorie limit"
+      hintText="2500 kCal per day is recommended for people of your age"
+      placeholder="Enter your calorie limit"
+      type="text"
+      feedback={errors.calories?.message}
+      {...register("calories", {
+        required: "This field is required",
+      })}
+       />
+      </Form>
     </div>
   )
 }
