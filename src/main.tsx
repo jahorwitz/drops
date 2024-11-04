3d8eb60f90956c660b51cf7c3a710d6fca526745
Brian Wang <brianwang7102@gmail.com>
2024-11-04 09:03:11 -0500

    feat: connected diet-goal-reminder to react router

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { DietGoalReminderList } from "./routes/onboarding/diet-goal-reminders-route";
 import "./global/default.css";
 import { GlucoseNotificationPrompt, Start, Welcome, RegistrationConfirmation } from "./routes";
+import { UseFormSetValue, FieldValues } from "react-hook-form";
 
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
@@ -19,6 +20,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <Route path="/" element={<div>Home Route</div>} />
           <Route path="/registration-confirm" element={<RegistrationConfirmation />} />
           <Route path="/onboarding/glucose-notifications" element={<GlucoseNotificationPrompt />} />
+          <Route path="/onboarding/diet-goal-reminder" element={<DietGoalReminderList onChange={function (): UseFormSetValue<FieldValues> {
+            //@ts-expect-error onChange function to be used with the onboarding wizard which has not been implemented yet
+            return;
+          } }/>}/>
           <Route path="/welcome" element={<Welcome />} />
           <Route path="/onboarding" element={<Start />} />
         </Routes >
