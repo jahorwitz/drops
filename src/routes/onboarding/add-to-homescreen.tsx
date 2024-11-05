27666f6bd370f381df4fb6913127d6cd6a7d0927
Darien Sessions <darien.sessions@gmail.com>
2024-11-05 08:14:20 -0500

    feat: add add-to-homescreen route and component

---
+++ src/routes/onboarding/add-to-homescreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Form } from "../../components"
+
+export const AddToHomescreen: React.FC = () => {
+  return (
+    <div>
+      <p>Test</p>
+      <Form></Form>
+    </div>
+  )
+}
