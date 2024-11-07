496db00f2fcfa82fc663f9f22e6501419b2f7007
Riley Marcum <rmarcum13@gmail.com>
2024-11-07 16:48:04 -0500

    style: adjustments to styling

--- src/routes/onboarding/glucose-notification-list.tsx
+++ src/routes/onboarding/glucose-notification-list.tsx
@@ -6,21 +6,15 @@ interface Props {
   elementId?: number;
 }
 
-export const GlucoseNotificationList: React.FC<Props> = ({
-  index,
-}) => {
-
+export const GlucoseNotificationList: React.FC<Props> = ({ index }) => {
   return (
     <div className="flex flex-col max-w-screen-md bg-lightPink relative overflow-hidden m-auto pb-80 h-screen">
-      <div className="flex flex-col gap-6 items-center pt-32 px-4">
+      <div className="flex flex-col self-center gap-6 items-center pt-32 px-4 max-w-[500px]">
         <h2 className="font-text text-section-header font-medium leading-[38.4px] text-center max-w-[358px] mx-auto">
           Get notified to measure glucose!
         </h2>
         <div className="w-full bg-white p-4 rounded-lg flex flex-col">
-          <Form.AddMoreSection
-            buttonText="+ Add more reminders"
-            className="pt-[30px]"
-          >
+          <Form.AddMoreSection buttonText="+ Add more" className="pt-[30px]">
             <Form.ListTimeInput label="Reminder" parentIndex={index} />
           </Form.AddMoreSection>
         </div>
