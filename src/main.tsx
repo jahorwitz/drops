3135604ed43fa7505617cb81023c8766ab4ccf59
RuvenPinkhasov <rubi00010@gmail.com>
2024-11-07 21:04:43 -0500

    feat: merged main

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import DietGoals from "./routes/onboarding/diet.goals";
 import "./global/default.css";
 import {
   GlucoseNotificationPrompt,
