32ecef90188a82f1636d10f14a53e8ea40852db7
Riley Marcum <rmarcum13@gmail.com>
2024-11-07 19:20:26 -0500

    style: spacing fixes

--- src/routes/onboarding/glucose-notification-list.tsx
+++ src/routes/onboarding/glucose-notification-list.tsx
@@ -13,8 +13,8 @@ export const GlucoseNotificationList: React.FC<Props> = ({ index }) => {
         <h2 className="font-text text-section-header font-medium leading-[38.4px] text-center max-w-[358px] mx-auto">
           Get notified to measure glucose!
         </h2>
-        <div className="w-full bg-white p-4 rounded-lg flex flex-col">
-          <Form.AddMoreSection buttonText="+ Add more" className="pt-[30px]">
+        <div className="w-full gap-5 bg-white p-4 rounded-lg flex flex-col">
+          <Form.AddMoreSection buttonText="+ Add more" className="p-[0]">
             <Form.ListTimeInput label="Reminder" parentIndex={index} />
           </Form.AddMoreSection>
         </div>
