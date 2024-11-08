cc5d68446f3ae2e9e4720d1199f3e7fc4dfeb66f
ItzLarz31 <larz9889@hotmail.com>
2024-11-07 20:15:59 -0500

    refactor: removed the header design

--- src/routes/registration/AccountCreationForm.tsx
+++ src/routes/registration/AccountCreationForm.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Form } from "../../components/form";
 import { Button } from "../../components/button";
-import logo from "../../images/LogoSmall.svg";
-import backbutton from "../../images/BackButton.svg";
 
 type FormValues = {
   name: string;
@@ -31,12 +29,6 @@ export const AccountCreationForm: React.FC = () => {
 
   return (
     <div className="pl-4 pr-4 bg-[#F5F5F5]">
-      <img src={backbutton} alt="Back Button" className="absolute top-1" />
-      <div className="flex justify-center flex-col text-center mb-5 mt-3">
-        <img src={logo} alt="Drop Logo" className="w-20 h-10 self-center" />
-        <h1 className="text-3xl font-medium pt-5 pb-2">Registration</h1>
-        <p>Step 1/2</p>
-      </div>
       <Form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
         <Form.TextInput
           labelText="Name"
