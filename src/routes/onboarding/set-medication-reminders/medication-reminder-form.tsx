1b5f735c00b8dba6046542e671f8448e9d4ef654
Caleb <chasch96@gmail.com>
2024-11-08 09:24:47 +0800

    fix: fix button spacing & background height

--- src/routes/onboarding/set-medication-reminders/medication-reminder-form.tsx
+++ src/routes/onboarding/set-medication-reminders/medication-reminder-form.tsx
@@ -4,7 +4,7 @@ import { MedicationSection } from "./medication-section";
 
 export const MedicationReminderForm: React.FC = () => {
   return (
-    <div className="flex flex-col bg-lightYellow max-w-screen-md pb-8 px-[10px] relative m-auto">
+    <div className="flex flex-col bg-lightYellow max-w-screen-md pb-8 px-[10px] relative m-auto h-screen">
       <h2 className="font-text text-section-header font-medium leading-[38.4px] text-center max-w-[358px] mx-auto">Set your medication reminders</h2>
       <p className="font-text text-center max-w-[358px] mx-auto mt-4 mb-8">It’s important to be consistent in your medications, and notifications may help!</p>
       <Form className="flex flex-col gap-5 ">
