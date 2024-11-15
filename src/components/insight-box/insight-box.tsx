260feb5c5895560584a228bc6a2867e648b90d49
RuvenPinkhasov <rubi00010@gmail.com>
2024-11-14 22:05:47 -0500

    feat: finished setting up the props for the component

---
+++ src/components/insight-box/insight-box.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import clsx from "clsx";
+
+export interface InsightProps {
+  title: string;
+  message: string;
+  icon?: React.ReactNode;
+  onClose?: () => void;
+}
+
+const baseStyles =
+  "w-full max-w-md flex flex-row justify-between items-center p-4 relative bg-white shadow-md rounded rounded-3xl";
+
+export const Insight: React.FC<InsightProps> = ({ title, message, icon }) => {
+  const variantStyles = clsx(baseStyles);
+
+  return (
+    <div className={variantStyles}>
+      <div className="flex flex-col gap-1 flex-grow">
+        <span className="text-xs text-gray-400 ">{title}</span>
+        <span className="mt-1 text-base text-black-500 break-words">
+          {message}
+        </span>
+      </div>
+      <div className="flex items-center justify-between">
+        {icon && <span className={`float-left`}>{icon}</span>}
+      </div>
+    </div>
+  );
+};
