ba4bd88da32d51514540575ec964ff11cd451a51
Caleb <chasch96@gmail.com>
2024-11-20 19:51:22 +0800

    style: fix padding

--- src/routes/settings/settings.tsx
+++ src/routes/settings/settings.tsx
@@ -2,11 +2,10 @@ import { Button } from "../../components"
 
 export const Settings: React.FC = () => {
   return (
-    <div className="flex flex-col bg-lightGray items-center gap-8">
-      <div className="flex flex-col items-center gap-8 my-[60px]">
+    <div className="flex flex-col bg-lightGray items-center pb-11">
         <Button
           variant="text"
-          className="opacity-100 hover:opacity-60 text-base max-w-max"
+          className="opacity-100 hover:opacity-60 text-base max-w-max pt-0"
           buttonText="Log out"
           onClick={() => alert("Log out")}
         />
@@ -16,7 +15,6 @@ export const Settings: React.FC = () => {
           buttonText="Delete profile"
           onClick={() => alert("Profile deleted!")}
         />
-      </div> 
     </div>
   )
 }
