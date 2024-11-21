962f6f401ef2b275a8c4ebe4a1ce5a74f6511f1a
Caleb <chasch96@gmail.com>
2024-11-22 01:57:21 +0800

    feat: add links & icons

--- src/routes/dashboard/header.tsx
+++ src/routes/dashboard/header.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import insights from "../../images/Insights.png";
+import notification from "../../images/Notifications.png";
+import avatar from "../../images/Avatar.png";
 
 export const Header: React.FC = () => {
   return (
-    <div>
-      <p>Hello</p>
+    <div className="flex justify-between bg-lightGray px-4 py-5">
+      <p>Hi, Rachel!</p>
+      <div className="flex gap-4">
+        <Link to="/insights">
+          <img src={insights} />
+        </Link>
+        <Link to="/notifications">
+          <img src={notification} />
+        </Link>
+        <Link to="/settings">
+          <img src={avatar} />
+        </Link>
+      </div>
     </div>
   )
 }
