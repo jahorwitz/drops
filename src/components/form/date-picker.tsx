14eeb81f4e79e88520fe3ece108ce7cd8de7cfcb
RuvenPinkhasov <rubi00010@gmail.com>
2024-11-21 22:41:08 -0500

    feat: added rest prop

--- src/components/form/date-picker.tsx
+++ src/components/form/date-picker.tsx
@@ -1,4 +1,4 @@
-import { HTMLProps, forwardRef, useState } from "react";
+import { forwardRef, useState } from "react";
 import cx from "classnames";
 import {
   UseFormRegisterReturn,
@@ -6,19 +6,29 @@ import {
   FieldValues,
 } from "react-hook-form";
 import DatePickerLib from "react-datepicker";
+import { DatePickerProps } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 type Props<T extends FieldValues> = UseFormRegisterReturn<string> &
-  HTMLProps<HTMLInputElement> & {
+  DatePickerProps & {
     labelText?: string;
     hintText?: string;
     feedback?: string;
     className?: string;
+    selectsRange?: never;
+    selectsMultiple?: never;
     setValue: UseFormSetValue<T>;
   };
 
 export const DatePicker = forwardRef<HTMLInputElement, Props<FieldValues>>(
-  ({ labelText, hintText, feedback, className }: Props<FieldValues>) => {
+  ({
+    labelText,
+    hintText,
+    feedback,
+    className,
+    onChange,
+    ...rest
+  }: Props<FieldValues>) => {
     const [startDate, setStartDate] = useState<Date | null>(null);
 
     return (
@@ -27,12 +37,18 @@ export const DatePicker = forwardRef<HTMLInputElement, Props<FieldValues>>(
         <DatePickerLib
           selected={startDate}
           placeholderText="MM/DD/YYYY"
-          onChange={(date) => setStartDate(date)}
+          onChange={(date) => {
+            setStartDate(date);
+            if (date) {
+              onChange?.(date);
+            }
+          }}
           popperPlacement="bottom-start"
           className={cx(
             `rounded-lg border-black border-[1px] py-5 px-3`,
             className,
           )}
+          {...rest}
         />
         {feedback ? (
           <span className="text-red text-base leading-5">{feedback}</span>
