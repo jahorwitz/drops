3146b19323ae6505f2ae1953b0e323849f9be34c
Riley Marcum <rmarcum13@gmail.com>
2024-11-21 19:50:14 -0500

    add dynamics to component

--- src/components/dashboard-metrics/metric-box-list.tsx
+++ src/components/dashboard-metrics/metric-box-list.tsx
@@ -2,16 +2,26 @@ import React from "react";
 import { MetricBox } from "./metric-box";
 
 interface MetricBoxListProps {
-  metricBoxes: React.ReactElement<typeof MetricBox>[];
+  metricBoxes: Array<{
+    variant: "Glucose" | "Medication" | "Activity" | "Mood" | "Diet";
+    boxColor?: "Yellow" | "Blue" | "Pink" | "Green" | "Purple";
+    promptVisible?: boolean;
+  }>;
 }
 
 export const MetricBoxList: React.FC<MetricBoxListProps> = ({
   metricBoxes,
 }) => {
   return (
-    <div className="grid grid-cols-2 gap-2">
+    <div className="grid grid-cols-2 w-[390px] gap-2 bg-lightGray p-4 rounded-lg">
       {metricBoxes.map((box) => {
-        return box;
+        return (
+          <MetricBox
+            variant={box.variant}
+            boxColor={box.boxColor}
+            promptVisible={box.promptVisible}
+          ></MetricBox>
+        );
       })}
     </div>
   );
--- src/components/dashboard-metrics/metric-box.tsx
+++ src/components/dashboard-metrics/metric-box.tsx
@@ -1,36 +1,79 @@
+//TODO: remove static image imports when dynamic graphs are in place
+import glucoseImage from "../../images/metric-image-glucose.svg";
+import activityImage from "../../images/metric-image-activity.svg";
+import dietImage from "../../images/metric-image-diet.svg";
+import moodImage from "../../images/metric-image-mood.svg";
+
 interface MetricBoxProps {
-  variant: "Glucose" | "Medication" | "Activity" | "Mood" | "Diet";
-  boxColor?: "Yellow" | "Blue" | "Pink" | "Green" | "Purple";
+  variant: string;
+  boxColor?: string;
   promptVisible?: boolean;
 }
 
+//TODO: import array of saved medication reminders or the reminder that is coming up next
+const medicationTimes = ["4:30p"];
+
 export const MetricBox = ({
   variant,
-  promptVisible = true,
+  promptVisible = false,
   boxColor = "Pink",
 }: MetricBoxProps) => {
   return (
     <div
-      className={`bg-light${boxColor} h-[180px] metric-card box-content rounded-2xl p-[10px] relative`}
+      className={`bg-light${boxColor} h-[160px] metric-card box-content rounded-2xl overflow-hidden p-[10px] relative`}
     >
       <h4 className="font-text text-section-subtext font-medium leading-[24px] mb-2">
         {variant}
       </h4>
       <p className="font-text text-black opacity-60 max-w-[140px] leading-[19.2px]">
-        {promptVisible &&
-          (variant === "Glucose"
+        {(variant === "Glucose" &&
+          (promptVisible
             ? "Set up reminders to measure glucose"
-            : variant === "Medication"
-            ? "Set up your medication to keep track of it"
-            : variant === "Diet"
-            ? "Set up your diet goals to keep track of them"
-            : variant === "Activity"
-            ? "Set up your activity goals to keep track of them"
-            : variant === "Mood"
-            ? "Record how you feel today"
-            : "")}
+            : "Measure at 4PM")) ||
+          (variant === "Medication" &&
+            (promptVisible
+              ? "Set up your medication to keep track of it"
+              : "Insulin 4 units at 4:30 PM")) ||
+          (variant === "Diet" &&
+            (promptVisible
+              ? "Set up your diet goals to keep track of them"
+              : "Have a snack at 5:30 PM")) ||
+          (variant === "Activity" &&
+            (promptVisible
+              ? "Set up your activity goals to keep track of them"
+              : "Excercise at 6 PM")) ||
+          (variant === "Mood" && "Record how you feel today")}
       </p>
-      <button className="absolute bottom-[10px] right-[10px]">+</button>
+      <img
+        //TODO: replace static images with dynamic graphs
+        src={
+          !promptVisible
+            ? (variant === "Glucose" && glucoseImage) ||
+              (variant === "Diet" && dietImage) ||
+              (variant === "Activity" && activityImage) ||
+              (variant === "Mood" && moodImage) ||
+              undefined
+            : undefined
+        }
+        className="absolute left-0 bottom-0"
+      ></img>
+      <p
+        className={`font-bold text-medication-reminder font-text bg-gradient-to-b from-yellow bg-clip-text text-transparent absolute -bottom-2 left-0 leading-[76.8px] ${
+          variant === "Medication" ? "block" : "hidden"
+        }`}
+      >
+        {medicationTimes[0]}
+      </p>
+      <button
+        className={`absolute bottom-[10px] right-[10px] ${
+          variant !== "Medication" ? "block" : "hidden"
+        } `}
+      >
+        <div className="relative flex items-center justify-center w-6 h-6 bg-white rounded-full">
+          <div className="absolute w-3/4 h-[2px] bg-black"></div>
+          <div className="absolute h-3/4 w-[2px] bg-black"></div>
+        </div>
+      </button>
     </div>
   );
 };
