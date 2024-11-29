a9670586cd04ba34f7c6132ec4f2d68244f19022
RuvenPinkhasov <rubi00010@gmail.com>
2024-11-29 08:32:45 -0500

    fix: fixed styling for back button

--- src/routes/notifications/notification-page.tsx
+++ src/routes/notifications/notification-page.tsx
@@ -3,12 +3,12 @@ import backbutton from "../../images/Backbutton.svg";
 
 export const Notifications: React.FC = () => {
   return (
-    <div className="cursor-pointer flex flex-col bg-lightGray overflow-auto max-w-screen-md pb-8 relative m-auto h-screen">
+    <div className="flex flex-col bg-lightGray overflow-auto max-w-screen-md pb-8 relative m-auto h-screen">
       <Link
         to="/"
-        className="z-10 ml-5 flex flex-row absolute top-[15px] left-4"
+        className="z-10 ml-5 flex flex-row absolute top-[15px] left-4 gap-1 cursor-pointer"
       >
-        <img src={backbutton} className="w-1 h-2" />
+        <img src={backbutton} className="mt-1 w-2 h-3" />
         <p className="text-paragraph-lg text-black leading-5">Back</p>
       </Link>
       <h2 className="text-section-subtext leading-6 text-black text-center mt-[15px] font-medium">
