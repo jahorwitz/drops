8607754bc8fea08694a5f154efa6cf83eefc52cc
RuvenPinkhasov <rubi00010@gmail.com>
2024-12-01 17:02:31 -0500

    refactor: prettier formatting

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {  Navigate } from 'react-router-dom';
-import { useAuth } from '../../hooks/useAuth';
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
