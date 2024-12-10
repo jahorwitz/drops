a102eff35b0faf132f99fcb0f3828b84a6308c6a
Riley Marcum <rmarcum13@gmail.com>
2024-12-10 08:40:46 -0500

    add addmoresection

--- src/routes/settings/profile-medications.tsx
+++ src/routes/settings/profile-medications.tsx
@@ -1,3 +1,11 @@
+import { Form } from "../../components";
+
+//create fake data to initally load into component
+
 export const ProfileMedications: React.FC = () => {
-  return <></>;
+  return (
+    <Form.AddMoreSection buttonText="+ Add medication">
+      <p>data</p>
+    </Form.AddMoreSection>
+  );
 };
