b4c2226f71444d28a6b06448163651e8a4324186
Brian Wang <brianwang7102@gmail.com>
2024-12-10 16:30:50 -0500

    feat: added stories

---
+++ src/routes/onboarding/set-diet-goal-reminders/set-diet-goals.stories.tsx
@@ -0,0 +1,12 @@
+import { SetDietGoals } from "./set-diet-goals";
+
+export default {
+    title: "Set Diet Goals",
+    component: SetDietGoals
+}
+
+export const Default = () => {
+    return (
+        <SetDietGoals/>
+    )
+}
