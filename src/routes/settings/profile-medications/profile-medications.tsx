24cc10a7d76d1140e362ed5401d3e7474d527419
Riley Marcum <rmarcum13@gmail.com>
2024-12-16 19:58:10 -0500

    add modal component

--- src/routes/settings/profile-medications/profile-medications.tsx
+++ src/routes/settings/profile-medications/profile-medications.tsx
@@ -1,5 +1,7 @@
 import { faPencil, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-import { Button } from "../../../components";
+import { Button, Modal } from "../../../components";
+import { useForm } from "react-hook-form";
+import useModal from "../../../components/modal/useModalHook";
 
 const medications = [
   {
@@ -12,7 +14,20 @@ const medications = [
   },
 ];
 
+interface MyFormValues {
+  myTextField: string;
+  anotherTextField: string;
+  anEnumField: "one" | "two" | "three";
+}
+
 export const ProfileMedications: React.FC = () => {
+  const { openModal } = useModal();
+
+  const { handleSubmit } = useForm<MyFormValues>();
+  const onSubmit = (data: MyFormValues) => {
+    alert(JSON.stringify(data, null, 2));
+  };
+
   return (
     <div className="rounded-2xl bg-white p-3 mx-[10px] my-3 text-center">
       <h4 className="font-text text-section-subtext font-medium pb-4 text-left">
@@ -28,13 +43,27 @@ export const ProfileMedications: React.FC = () => {
               </p>
             </div>
             <div>
-              <Button variant="icon" icon={faPencil} onClick={() => {}} />
+              <Button
+                variant="icon"
+                icon={faPencil}
+                onClick={() => {
+                  openModal("profile-medications-edit");
+                }}
+              />
               <Button variant="icon" icon={faTrashAlt} onClick={() => {}} />
             </div>
           </div>
         );
       })}
       <button className="font-text font-medium">+ Add medication</button>
+      <Modal
+        modalId="profile-medications-edit"
+        title="Editing medication"
+        buttonText="Save"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        Name and Reminders here
+      </Modal>
     </div>
   );
 };
