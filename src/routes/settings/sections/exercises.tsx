cffa1ff04b392648463e9531a5968d4fe04bf9a7
Riley Marcum <rmarcum13@gmail.com>
2025-01-29 21:24:49 -0500

    Add Exercises component for managing exercise entries

---
+++ src/routes/settings/sections/exercises.tsx
@@ -0,0 +1,27 @@
+import { Form } from "../../../components";
+import { useAuth } from "../../../hooks/useAuth";
+
+export default function Exercises() {
+  const user = useAuth({});
+  console.log(user);
+
+  return (
+    <div className="bg-gray-300 h-screen">
+      <div className="flex flex-col gap-4 rounded-xl bg-white p-3 max-w-lg">
+        <h4 className="leading-6 font-medium text-xl">Excercises</h4>
+        <Form.AddMoreSection buttonText="+ Add exercise">
+          <div className="flex justify-between items-center">
+            <div>
+              <p>Walk</p>
+              <p className="text-[rgba(18,18,18,0.6)]">Daily, 9:00 AM</p>
+            </div>
+            <div className="flex gap-3">
+              <button>E</button>
+              <button>D</button>
+            </div>
+          </div>
+        </Form.AddMoreSection>
+      </div>
+    </div>
+  );
+}
