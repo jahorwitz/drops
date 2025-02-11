7c464b91fc2414d23ed7d2d4996eea332dc81af2
rebecca justice <rebecca.bindraban@gmail.com>
2025-02-11 13:46:55 -0700

    feat: aaded account detail form

---
+++ src/routes/registration/AccountDetailForm.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { Form } from "../../components/form";
+
+interface FormValues {
+  diabetesType: string;
+  dateOfBirth: string;
+  weight: string;
+  height: string;
+  sex: string;
+    
+}
+
+
+
+export const AccountDetailForm: React.FC = () => {
+    const {
+      register,
+      handleSubmit,
+      control,
+      watch,
+      formState: { errors },
+    } = useForm<FormValues>();
+  
+    const onSubmit = (data: FormValues) => {
+      alert(JSON.stringify(data, null, 2));
+    };
+  
+    return (
+       <div className="pl-4 pr-4 bg-[#F5F5F5]">
+      <Form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 bg-white">
+      <Form.RadioGroup 
+          labelText="Choose your diabetes type"
+          hintText="Select one option"
+          options={[
+            { value: "type2", label: "Type 2" },
+            { value: "type1", label: "Type 1" },
+            { value: "gestational", label: "Gestational" },
+          ]}
+          value={watch("diabetesType")} 
+          feedback={errors as FieldErrors}
+          {...register("diabetesType", {
+            required: "This field is required",
+          })}
+        />
+         <Controller
+        name="dateValue"
+        control={control}
+        render={({ field: { onChange, value, ...field } }) => (
+          <Form.DatePicker
+            {...field}
+            value={value}
+            onChange={onChange}
+            labelText="Date of birth"
+            hintText="Enter a date in MM/DD/YYYY format"
+            feedback={errors.dateValue?.message}
+            name="dateValue"
+          />
+        )}
+      />
+       <Form.SelectForm
+        labelText="Sex"
+        placeholder="Choose"
+        hintText="Select one option"
+        options={[
+          { value: "male", label: "Male" },
+          { value: "female", label: "Female" },
+          { value: "other", label: "Other" },
+        ]}
+        feedback={errors as FieldErrors}
+        {...register("sex", {
+          required: "This field is required",
+        })}
+      />
+    <Form.TextInput
+          labelText="Weight (lbs)"
+          placeholder="Enter your weight"
+          type="text"
+          feedback={errors.weight?.message}
+          filled={`${!watch("weight") ? "filled" : ""}`}
+          {...register("weight")}
+        />
+       <Form.TextInput
+          labelText="Height (ft)"
+          placeholder={`Enter your height in ft'in"`}
+          type="text"
+          feedback={errors.height?.message}
+          filled={`${!watch("height") ? "filled" : ""}`}
+          {...register("height")}
+        />
+        <p className="font-text opacity-60">Your data is needed to provide correct pieces of advice</p>
+      </Form>
+      </div>
+    );
+  };
+
--- src/routes/registration/index.ts
+++ src/routes/registration/index.ts
@@ -1,2 +1,3 @@
 export * from "./registration-confirm";
 export * from "./AccountCreationForm";
+export * from "./AccountDetailForm";
