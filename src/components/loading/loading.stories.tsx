6b43ffb08a1227562c4b006fb73f0a0a5bbac1f1
Joshua Horwitz <jahorwitz@Joshuas-MBP-2.lan>
2025-04-22 19:41:20 -0400

    fix: storybook crash

--- src/components/loading/loading.stories.tsx
+++ src/components/loading/loading.stories.tsx
@@ -1,5 +1,17 @@
+import { Meta, StoryFn } from "@storybook/react";
 import { LoadingCircle } from "./loading-circle";
+import { HiLightBulb } from "react-icons/hi";
 
-export const BlueLoader = () => {
-  <LoadingCircle />
-}
+export default {
+  title: "Loading",
+  component: LoadingCircle,
+} as Meta;
+
+const Template: StoryFn = () => <LoadingCircle />;
+
+export const InsightBox = Template.bind({});
+InsightBox.args = {
+  title: "You need to exercise more!",
+  message: "Try walking for at least 1 hour per day",
+  icon: <HiLightBulb className="w-10 h-10 fill-yellow" />,
+};
