f4da2aec9b39be365d53cf52db63241dad2e359b
Trang T <trangmtruong@yahoo.com>
2025-05-09 23:25:01 -0700

    feat(diet-logs): add meal logs with styles

--- src/__generated__/gql.ts
+++ src/__generated__/gql.ts
@@ -11,11 +11,30 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * 3. It does not support dead code elimination, so it will add unused operations.
  *
  * Therefore it is highly recommended to use the babel or swc plugin for production.
+ * Learn more about it here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#reducing-bundle-size
  */
-const documents = {
+type Documents = {
+    "\n  query GetReminders {\n    authenticatedItem {\n      ... on User {\n        id\n        reminders {\n          id\n          time\n          label\n          type\n        }\n      }\n    }\n  }\n": typeof types.GetRemindersDocument,
+    "\n  mutation CreateReminder($data: ReminderCreateInput!) {\n    createReminder(data: $data) {\n      id\n      time\n      label\n      type\n    }\n  }\n": typeof types.CreateReminderDocument,
+    "\n  mutation DeleteReminder($where: ReminderWhereUniqueInput!) {\n    deleteReminder(where: $where) {\n      id\n    }\n  }\n": typeof types.DeleteReminderDocument,
+    "\n  mutation UpdateReminder($where: ReminderWhereUniqueInput!, $data: ReminderUpdateInput!) {\n    updateReminder(where: $where, data: $data) {\n      id\n      time\n    }\n  }\n": typeof types.UpdateReminderDocument,
+    "\n  mutation AuthenticateUserWithPassword($email: String!, $password: String!) {\n    authenticateUserWithPassword(email: $email, password: $password) {\n      ... on UserAuthenticationWithPasswordSuccess {\n        sessionToken\n        item {\n          id\n          name\n          email\n          isOnboardingComplete\n          isRegistrationComplete\n        }\n      }\n      ... on UserAuthenticationWithPasswordFailure {\n        message\n      }\n    }\n  }\n": typeof types.AuthenticateUserWithPasswordDocument,
+    "\n  mutation Mutation { \n    endSession\n  }": typeof types.MutationDocument,
+    "\n  mutation UpdateUser($where: UserWhereUniqueInput!, $data: UserUpdateInput!) {\n    updateUser(where: $where, data: $data) {\n      name\n      email\n      height\n      sex\n      weight\n      dateOfBirth\n      diabetesType\n    }\n  }\n": typeof types.UpdateUserDocument,
+    "query DietLogs($where: DietLogWhereInput!, $orderBy: [DietLogOrderByInput!]!) {\n  dietLogs(where: $where, orderBy: $orderBy) {\n    mealName\n    mealDescription\n    protein\n    sugar\n    calories\n    carbs\n    fiber\n    logTime\n  }\n}": typeof types.DietLogsDocument,
+    "\n  query NewNotifications {\n    notifications(\n      where: {\n        status: { equals: \"new\" },\n        notificationTime: {\n          gte: \"\"\n          lte: \"\"\n        }\n      }\n      orderBy: [{ notificationTime: asc }]\n    ) {\n      notificationTime\n      status\n      type\n      description\n    }\n  }\n": typeof types.NewNotificationsDocument,
+    "\n  query OldNotifications {\n    notifications(\n      where: {\n        status: { equals: \"archived\" },\n        notificationTime: {\n          gte: \"\"\n          lte: \"\"\n        }\n      }\n      orderBy: [{ notificationTime: desc }]\n    ) {\n      notificationTime\n      status\n      type\n      description\n    }\n  }\n": typeof types.OldNotificationsDocument,
+    "\n  query User($where: NotificationWhereInput) {\n    authenticatedItem {\n      ... on User {\n        name\n        email\n        isOnboardingComplete\n        isRegistrationComplete\n        notificationsCount(where: $where)\n        dateOfBirth\n        diabetesType\n        height\n        sex\n        weight\n      }\n    }\n  }\n": typeof types.UserDocument,
+};
+const documents: Documents = {
+    "\n  query GetReminders {\n    authenticatedItem {\n      ... on User {\n        id\n        reminders {\n          id\n          time\n          label\n          type\n        }\n      }\n    }\n  }\n": types.GetRemindersDocument,
+    "\n  mutation CreateReminder($data: ReminderCreateInput!) {\n    createReminder(data: $data) {\n      id\n      time\n      label\n      type\n    }\n  }\n": types.CreateReminderDocument,
+    "\n  mutation DeleteReminder($where: ReminderWhereUniqueInput!) {\n    deleteReminder(where: $where) {\n      id\n    }\n  }\n": types.DeleteReminderDocument,
+    "\n  mutation UpdateReminder($where: ReminderWhereUniqueInput!, $data: ReminderUpdateInput!) {\n    updateReminder(where: $where, data: $data) {\n      id\n      time\n    }\n  }\n": types.UpdateReminderDocument,
     "\n  mutation AuthenticateUserWithPassword($email: String!, $password: String!) {\n    authenticateUserWithPassword(email: $email, password: $password) {\n      ... on UserAuthenticationWithPasswordSuccess {\n        sessionToken\n        item {\n          id\n          name\n          email\n          isOnboardingComplete\n          isRegistrationComplete\n        }\n      }\n      ... on UserAuthenticationWithPasswordFailure {\n        message\n      }\n    }\n  }\n": types.AuthenticateUserWithPasswordDocument,
     "\n  mutation Mutation { \n    endSession\n  }": types.MutationDocument,
     "\n  mutation UpdateUser($where: UserWhereUniqueInput!, $data: UserUpdateInput!) {\n    updateUser(where: $where, data: $data) {\n      name\n      email\n      height\n      sex\n      weight\n      dateOfBirth\n      diabetesType\n    }\n  }\n": types.UpdateUserDocument,
+    "query DietLogs($where: DietLogWhereInput!, $orderBy: [DietLogOrderByInput!]!) {\n  dietLogs(where: $where, orderBy: $orderBy) {\n    mealName\n    mealDescription\n    protein\n    sugar\n    calories\n    carbs\n    fiber\n    logTime\n  }\n}": types.DietLogsDocument,
     "\n  query NewNotifications {\n    notifications(\n      where: {\n        status: { equals: \"new\" },\n        notificationTime: {\n          gte: \"\"\n          lte: \"\"\n        }\n      }\n      orderBy: [{ notificationTime: asc }]\n    ) {\n      notificationTime\n      status\n      type\n      description\n    }\n  }\n": types.NewNotificationsDocument,
     "\n  query OldNotifications {\n    notifications(\n      where: {\n        status: { equals: \"archived\" },\n        notificationTime: {\n          gte: \"\"\n          lte: \"\"\n        }\n      }\n      orderBy: [{ notificationTime: desc }]\n    ) {\n      notificationTime\n      status\n      type\n      description\n    }\n  }\n": types.OldNotificationsDocument,
     "\n  query User($where: NotificationWhereInput) {\n    authenticatedItem {\n      ... on User {\n        name\n        email\n        isOnboardingComplete\n        isRegistrationComplete\n        notificationsCount(where: $where)\n        dateOfBirth\n        diabetesType\n        height\n        sex\n        weight\n      }\n    }\n  }\n": types.UserDocument,
@@ -38,6 +57,22 @@ export function gql(source: string): unknown;
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
+export function gql(source: "\n  query GetReminders {\n    authenticatedItem {\n      ... on User {\n        id\n        reminders {\n          id\n          time\n          label\n          type\n        }\n      }\n    }\n  }\n"): (typeof documents)["\n  query GetReminders {\n    authenticatedItem {\n      ... on User {\n        id\n        reminders {\n          id\n          time\n          label\n          type\n        }\n      }\n    }\n  }\n"];
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n  mutation CreateReminder($data: ReminderCreateInput!) {\n    createReminder(data: $data) {\n      id\n      time\n      label\n      type\n    }\n  }\n"): (typeof documents)["\n  mutation CreateReminder($data: ReminderCreateInput!) {\n    createReminder(data: $data) {\n      id\n      time\n      label\n      type\n    }\n  }\n"];
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n  mutation DeleteReminder($where: ReminderWhereUniqueInput!) {\n    deleteReminder(where: $where) {\n      id\n    }\n  }\n"): (typeof documents)["\n  mutation DeleteReminder($where: ReminderWhereUniqueInput!) {\n    deleteReminder(where: $where) {\n      id\n    }\n  }\n"];
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n  mutation UpdateReminder($where: ReminderWhereUniqueInput!, $data: ReminderUpdateInput!) {\n    updateReminder(where: $where, data: $data) {\n      id\n      time\n    }\n  }\n"): (typeof documents)["\n  mutation UpdateReminder($where: ReminderWhereUniqueInput!, $data: ReminderUpdateInput!) {\n    updateReminder(where: $where, data: $data) {\n      id\n      time\n    }\n  }\n"];
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
 export function gql(source: "\n  mutation AuthenticateUserWithPassword($email: String!, $password: String!) {\n    authenticateUserWithPassword(email: $email, password: $password) {\n      ... on UserAuthenticationWithPasswordSuccess {\n        sessionToken\n        item {\n          id\n          name\n          email\n          isOnboardingComplete\n          isRegistrationComplete\n        }\n      }\n      ... on UserAuthenticationWithPasswordFailure {\n        message\n      }\n    }\n  }\n"): (typeof documents)["\n  mutation AuthenticateUserWithPassword($email: String!, $password: String!) {\n    authenticateUserWithPassword(email: $email, password: $password) {\n      ... on UserAuthenticationWithPasswordSuccess {\n        sessionToken\n        item {\n          id\n          name\n          email\n          isOnboardingComplete\n          isRegistrationComplete\n        }\n      }\n      ... on UserAuthenticationWithPasswordFailure {\n        message\n      }\n    }\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
@@ -47,6 +82,10 @@ export function gql(source: "\n  mutation Mutation { \n    endSession\n  }"): (t
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function gql(source: "\n  mutation UpdateUser($where: UserWhereUniqueInput!, $data: UserUpdateInput!) {\n    updateUser(where: $where, data: $data) {\n      name\n      email\n      height\n      sex\n      weight\n      dateOfBirth\n      diabetesType\n    }\n  }\n"): (typeof documents)["\n  mutation UpdateUser($where: UserWhereUniqueInput!, $data: UserUpdateInput!) {\n    updateUser(where: $where, data: $data) {\n      name\n      email\n      height\n      sex\n      weight\n      dateOfBirth\n      diabetesType\n    }\n  }\n"];
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "query DietLogs($where: DietLogWhereInput!, $orderBy: [DietLogOrderByInput!]!) {\n  dietLogs(where: $where, orderBy: $orderBy) {\n    mealName\n    mealDescription\n    protein\n    sugar\n    calories\n    carbs\n    fiber\n    logTime\n  }\n}"): (typeof documents)["query DietLogs($where: DietLogWhereInput!, $orderBy: [DietLogOrderByInput!]!) {\n  dietLogs(where: $where, orderBy: $orderBy) {\n    mealName\n    mealDescription\n    protein\n    sugar\n    calories\n    carbs\n    fiber\n    logTime\n  }\n}"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
--- src/routes/viewing-logs/diet/diet-logs.tsx
+++ src/routes/viewing-logs/diet/diet-logs.tsx
@@ -1,60 +1,201 @@
 import React from "react";
-// import { useEffect, useState } from "react";
-// import { useQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
 import {Link} from "react-router-dom"
 import backbutton from '../../../images/Backbutton.svg';
 import settingLogo from "../../../images/Setting-logo.svg";
 import {Button} from "../../../components";
+import { DIET_LOGS } from "../../../graphql/queries/diet-log";
+import { useQuery } from "@apollo/client";
+import { DietLogsQuery } from "../../../__generated__/graphql";
 
-// interface DietLogsData {
-//     logTime: string,
-//     mealType: string,
-//     mealDescription: string,
-// }
+type DietLog = NonNullable<NonNullable<DietLogsQuery["dietLogs"]>[number]>;
+
+const now = new Date();
+const oneDay = 24*60*60*1000;
+
+const dayBefore = new Date(now.getTime() - oneDay).toISOString();
+// const dayAfter = new Date(now.getTime() + oneDay).toISOString();
+
+const formatLocalTime = (isoString: string): string => {
+  return new Date(isoString).toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  })
+}
 
 
-// useEffect (() => {})
-//filter logs in date buckets (Today & Last Date)
  
 export const DietLogs: React.FC = () => {
-    // const [upcomingMeals, setUpcomingMeals] = useState<DietLogsData[]>([],);
-    // const [pastMeals, setPastMeals] = useState<DietLogsData[]>([],);
+    // const [upcomingDietLogs, setUpcomingDietLogs] = useState<DietLog[]>([],);
+    const [pastTodayDietLogs, setPastTodayDietLogs] = useState<DietLog[]>([],);
+    const [pastBeforeTodayDietLogs, setPastBeforeTodayDietLogs ] = useState<DietLog[]>([],);
 
-   //add useQuery for diet logs
+
+  //  const{data: upcomingDietData,
+  //       loading: upcomingDietLoading,
+  //       error: upcomingDietError,
+  //  } = useQuery<DietLogsQuery>(DIET_LOGS, {
+  //   variables: {
+  //     "where": {
+  //       "logTime": {
+  //         "lte": dayAfter,
+  //         "gte": now.toISOString(),
+  //       }
+  //     },
+  //     "orderBy": [
+  //       {
+  //         "logTime": "asc"
+  //       }
+  //     ]
+  //   },
+  // });
+   const{data: pastTodayDietData,
+    loading: pastTodayDietLoading,
+        error: pastTodayDietError,
+   } =useQuery<DietLogsQuery>(DIET_LOGS, {
+    variables: {
+      "where": {
+        "logTime": {
+          "lte": now.toISOString(),
+          "gte": dayBefore,
+        }
+      },
+      "orderBy": [
+        {
+          "logTime": "desc"
+        }
+      ]
+    },
+  });
+   const{data: pastBeforeTodayDietData,
+    loading: pastBeforeTodayDietLoading,
+        error: pastBeforeTodayDietError,
+   } = useQuery<DietLogsQuery>(DIET_LOGS, {
+    variables: {
+      "where": {
+        "logTime": {
+          "lte": dayBefore,
+          
+        }
+        
+      },
+      "orderBy": [
+        {
+          "logTime": "desc"
+        }
+      ]
+    },
+  })
+  
+
+  useEffect(() => {
+    // if (upcomingDietData?.dietLogs) {
+    //   setUpcomingDietLogs(upcomingDietData.dietLogs);
+    // }
+
+    if(pastTodayDietData?.dietLogs){
+
+      setPastTodayDietLogs(pastTodayDietData.dietLogs);
+    }
+    if(pastBeforeTodayDietData?.dietLogs){
+      setPastBeforeTodayDietLogs(pastBeforeTodayDietData.dietLogs)
+    }
+    //add upcomingDietData
+  },[pastTodayDietData,pastBeforeTodayDietData]);
+
+//add upcomingDietLoading ||
+  if ( pastTodayDietLoading || pastBeforeTodayDietLoading) {
+    return <p className="text-center mt-10">Loading...</p>;
+  }
+  //add upcomingDietError || 
+  if (pastTodayDietError || pastBeforeTodayDietError) {
+    return (
+      <p className="text-center mt-10">
+        An error occurred while fetching diet logs.
+      </p>
+    );
+  }
+  
+
+  
   return (
-     <div className="flex flex-col bg-lightGray overflow-auto max-w-screen-md pb-8 relative m-auto h-screen">
+     <div className="flex flex-col bg-lightGreen overflow-auto max-w-screen-md pb-8 relative m-auto h-screen">
+        <div className="relative flex items-center py-[13px]">
           <Link
             to="/dashboard"
-            className="z-10 flex flex-row absolute top-[15px] left-4 gap-1 cursor-pointer"
+            className="flex flex-row absolute top-[15px] left-4 -translate-y-1/2 gap-1 items-center cursor-pointer"
           >
             <img src={backbutton} className="mt-1 w-2 h-3" />
             <p className="text-paragraph-lg text-black leading-5">Back</p>
           </Link>
-          <h1 className="text-section-subtext leading-6 text-black text-center mt-[15px] font-medium">
+          <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-section-subtext leading-6 text-black text-center mt-[15px] font-medium">
             Diet
           </h1>
           <Link
         //   fix to route to diet settings instead
-            to="/"
-            className="z-10 flex flex-row absolute cursor-pointer"
+            to="/diet/settings"
+            className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
           >
-            <img src={settingLogo} className="mt-1 w-2 h-3" />
+            <img src={settingLogo} className="mt-1 w-[18px] h-[20px]" />
             
           </Link>
+        </div>
           {/* Graph */}
-          <h2 className="text-paragraph-lg text-black leading-5 opacity-60 pt-7 pl-2.5">
-            Upcoming Meals & Snacks
+          <h2 className="text-paragraph-sm text-black leading-5 opacity-60 pt-7 pl-2.5">
+            UPCOMING MEALS & SNACKS
           </h2>
-          {/* a div to map upcoming snacks */}
+          {/* a div to map upcoming snacks 
+            returns remainder of the meals and snack that hasnt been logged for the day
+          */}
 
          {/* When click in Button, takes to a Record a Meal form */}
-          <Button type="submit" buttonText="Record a Meal" variant="primary" className="h-[60px] w-full mt-24 mb-8" />
-          <h2 className="text-paragraph-lg text-black leading-5 opacity-60 pt-8 pl-2.5">
-            Past Meals & Snacks - Today </h2>
-          {/* a div to map past meals and snacks for today */}
-          <h2 className="text-paragraph-lg text-black leading-5 opacity-60 pt-8 pl-2.5">
-          Past Meals & Snacks - Date </h2>
-          {/* a div to map past deals and snacks in the past */}
+          <Button type="submit" buttonText="Record a Meal" variant="primary" className="h-[60px] w-full" />
+          <h2 className="text-paragraph-sm text-black leading-5 opacity-60 pt-8 pl-2.5">
+            PAST MEALS & SNACKS - TODAY </h2>
+         
+              {/* a div to map past meals and snacks for today */}
+          <div className="flex flex-col gap-2 px-2.5 pt-3">
+          {/* if pastTodayDietLogs is > 0, map the past meals today logs */}
+              {pastTodayDietLogs.length > 0 ? (
+              pastBeforeTodayDietLogs.map((dietlog) => (
+              <div className="flex flex-col gap-1 flex-grow bg-white rounded-[16px] p-[12px_16px]">
+                <div className="flex justify-between items-start">
+                  <h3 className="text-paragraph-sm leading-[120%] text-black font-text break-words ">{dietlog.mealName} — {dietlog.mealDescription} </h3>
+                  <p className="text-paragraph-sm leading-[-120] text-black opacity-60  font-text">{formatLocalTime(dietlog.logTime).toLowerCase()}</p>
+                </div>
+                <p className="text-paragraph-sm leading-[-120] text-black opacity-60  font-text">{dietlog.calories} kCal, {dietlog.protein}g protein, {dietlog?.carbs} carbs, {dietlog?.fiber}g fiber, {dietlog?.sugar}g sugar</p>
+              </div>
+              )
+              )) : 
+              
+              // else, shows p tags that says "you forgot to log your meals and snack today"
+              
+              (
+              <p className="text-sm text-gray-500">You forgot to log your meals and snack today</p>
+              ) }
+          </div>
+          <h2 className="text-paragraph-sm text-black leading-5 opacity-60 pt-8 pl-2.5">
+          PAST MEALS & SNACKS - DATE </h2>
+              {/* a div to map past meals and snacks for before today */}
+          <div className="flex flex-col gap-2 px-2.5 pt-3">
+          {/* if pastTodayDietLogs is > 0, map the past meals before today logs */}
+              {pastBeforeTodayDietLogs.length > 0 ? (
+              pastBeforeTodayDietLogs.map((dietlog) => (
+              <div className="flex flex-col gap-1 flex-grow bg-white rounded-[16px] p-[12px_16px]">
+                <div className="flex justify-between items-start">
+                  <h3 className="text-paragraph-sm leading-[120%] text-black font-text break-words ">{dietlog.mealName} — {dietlog.mealDescription}</h3>
+                  <p className="text-paragraph-sm leading-[-120] text-black opacity-60  font-text">{formatLocalTime(dietlog.logTime).toLowerCase()}</p>
+                </div>
+                <p className="text-paragraph-sm leading-[-120] text-black opacity-60  font-text">{dietlog.calories} kCal, {dietlog.protein}g protein, {dietlog?.carbs} carbs, {dietlog?.fiber}g fiber, {dietlog?.sugar}g sugar</p>
+              </div>
+              )
+              )) : 
+              // else, shows p tags that says "you forgot to log your meals and snacks"
+              (
+              <p className="text-sm text-gray-500">You forgot to log your meals and snacks</p>
+              ) }
+          </div>
 
             
         
